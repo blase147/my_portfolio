946a5b083a8ed2e98d2fcc5aff05b46216fc1cac
blase147 <solarmails2@gmail.com>
2023-06-13 15:49:38 +0100

    Add basic files

--- src/components/contact-me/contactMe.js
+++ src/components/contact-me/contactMe.js
@@ -3,16 +3,16 @@ import './contactMe.scss';
 // import NavigationBar from '../navigation-bar/navigationBar';
 
 const ContactMe = () => (
-  <section>
-    {/* <NavigationBar /> */}
+  <section className="contact-me-container">
+    <div><h1>Get in Touch</h1></div>
     <div className="contact-me">
       <p>
         I am always interested in hearing about new projects,
         so if you would like to chat please get in touch.
 
       </p>
-      <div className="get-in-touch">
-        <form id="form" action="https://formspree.io/f/xqkjanrn" method="POST">
+      <div>
+        <form className="get-in-touch" id="form" action="https://formspree.io/f/xqkjanrn" method="POST">
           <input type="text" name="fullname" maxLength="30" placeholder="Fullname" required />
           <input id="mail" className="email" type="email" name="email" placeholder="Email address" />
           <textarea name="comment" cols="30" rows="10" maxLength="500" placeholder="Enter text here" required />
