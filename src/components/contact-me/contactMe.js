7bc0b7dbff93d58aa3ba7c28a7d9ed957621f883
blase147 <solarmails2@gmail.com>
2023-06-13 16:44:03 +0100

    Add basic files

--- src/components/contact-me/contactMe.js
+++ src/components/contact-me/contactMe.js
@@ -11,14 +11,14 @@ const ContactMe = () => (
         so if you would like to chat please get in touch.
 
       </p>
-      <div>
+      <div className="form-group">
         <form className="get-in-touch" id="form" action="https://formspree.io/f/xqkjanrn" method="POST">
           <input type="text" name="fullname" maxLength="30" placeholder="Fullname" required />
           <input id="mail" className="email" type="email" name="email" placeholder="Email address" />
           <textarea name="comment" cols="30" rows="10" maxLength="500" placeholder="Enter text here" required />
           <span className="message" />
-          <div className="align-left">
-            <button type="submit">Get in touch</button>
+          <div>
+            <button className="get-in-touch-btn" type="submit">Get in touch now!!</button>
           </div>
         </form>
       </div>
