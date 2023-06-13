d62563b510e6d79775c6f4cb18793ee0e26c57cd
blase147 <solarmails2@gmail.com>
2023-06-13 16:47:26 +0100

    Add basic files

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,26 +2,32 @@ import React from 'react';
 import './footer.scss';
 
 const Footer = () => (
-  <div className="footer">
-    <ul className="social-icons">
-      <li>
-        <a href="https://github.com/blase147"><img src="img/Icon-GitHub2.png" alt="icon" /></a>
-      </li>
-      <li>
-        <a href="www.linkedin.com/in/chukwuma-mosanya-346453"><img src="img/Icon-Linkedin.png" alt="icon" /></a>
-      </li>
-      <li>
-        <a href="www.angelist.com"><img src="img/Icon-Angelist2.png" alt="icon" /></a>
-      </li>
-      <li>
-        <a href="https://twitter.com/ChukwumaMosanya"><img src="img/Icon-Twitter2.png" alt="icon" /></a>
-      </li>
-      <li>
-        <a href="www.medium.com"><img src="img/Icon-Medium2.png" alt="icon" /></a>
-      </li>
-    </ul>
-
-  </div>
+  <section>
+    <div className="footer">
+      <div className="social-icons-container">
+        <ul className="social-icons">
+          <li>
+            <a href="https://github.com/blase147"><FaGithub size={45} alt="social-icon" /></a>
+          </li>
+          <li>
+            <a href="www.linkedin.com/in/chukwuma-mosanya-346453"><FaLinkedin size={45} alt="social-icon" /></a>
+          </li>
+          <li>
+            <a href="www.angelist.com"><FaAngellist size={45} alt="social-icon" /></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/ChukwumaMosanya"><FaTwitter size={45} alt="social-icon" /></a>
+          </li>
+          <li>
+            <a href="www.facebook.com"><FaFacebook size={45} alt="social-icons" /></a>
+          </li>
+        </ul>
+      </div>
+      <div className="profile-pic">
+        <img src="../../main photo.jpg" alt="profile-pic" />
+      </div>
+    </div>
+  </section>
 );
 
 export default Footer;
