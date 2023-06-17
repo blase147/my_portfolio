d868b1785bcccdd9fce096a2bbce9dc1c979c614
blase147 <solarmails2@gmail.com>
2023-06-17 09:08:05 +0100

    Add desktop responsiveness

--- src/components/about-me/aboutMe.js
+++ src/components/about-me/aboutMe.js
@@ -14,43 +14,48 @@ const AboutMe = () => (
     <div className="about-me-container">
       <section id="about" className="about">
         <div className="about-me">
+          <FontAwesomeIcon className="custom-icon" icon={faUserLarge} />
+          <h1>About me</h1>
           <div className="about-me-intro">
-            <FontAwesomeIcon className="custom-icon" icon={faUserLarge} />
-            <h1>About me</h1>
-            <p>
-              Hello I am a software developer! I can help you build a product,
-              feature or website Look through some of my work and experience!
-              If you like what you see and have a project you need coded,
-              do not hestiate to contact me.I am a software designer, with over
-              ten years of experience. Built up while working with teams big
-              and small across the United States, Japan and Australia.
+            <div className="intro-text">
+              <p>
+                Hello I am a software developer! I can help you build a product,
+                feature or website Look through some of my work and experience!
+                If you like what you see and have a project you need coded,
+                do not hestiate to contact me.I am a software designer, with over
+                ten years of experience. Built up while working with teams big
+                and small across the United States, Japan and Australia.
 
-              Now, I am back home. Living in Canterbury, New Zealand, where I spend my
-              free time reading about the history of video game development and riding track bikes.
+                Now, I am back home. Living in Canterbury, New Zealand, where I spend my
+                free time reading about the history of video game development and riding
+                track bikes.
 
-            </p>
-            <img src="./main-photo.jpg" alt="profile" />
-            <div className="buttons">
-              <DownloadPDF />
-              <a href="mailto:solarmails2gmail.com" target="_blank" rel="noreferrer">solarmails2@gmail.com</a>
+              </p>
             </div>
-            <ul className="social-icons">
-              <li>
-                <a href="https://github.com/blase147"><FaGithub size={30} alt="social-icon" /></a>
-              </li>
-              <li>
-                <a href="www.linkedin.com/in/chukwuma-mosanya-346303"><FaLinkedin size={30} alt="social-icon" /></a>
-              </li>
-              <li>
-                <a href="www.angelist.com"><FaAngellist size={30} alt="social-icon" /></a>
-              </li>
-              <li>
-                <a href="https://twitter.com/ChukwumaMosanya"><FaTwitter size={30} alt="social-icon" /></a>
-              </li>
-              <li>
-                <a href="www.facebook.com"><FaFacebook size={30} alt="social-icons" /></a>
-              </li>
-            </ul>
+            <div>
+              <img src="./main-photo.jpg" alt="profile" />
+              <div className="buttons">
+                <DownloadPDF />
+                <a href="mailto:solarmails2gmail.com" target="_blank" rel="noreferrer">solarmails2@gmail.com</a>
+              </div>
+              <ul className="social-icons">
+                <li>
+                  <a href="https://github.com/blase147"><FaGithub size={30} alt="social-icon" /></a>
+                </li>
+                <li>
+                  <a href="www.linkedin.com/in/chukwuma-mosanya-346303"><FaLinkedin size={30} alt="social-icon" /></a>
+                </li>
+                <li>
+                  <a href="www.angelist.com"><FaAngellist size={30} alt="social-icon" /></a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/ChukwumaMosanya"><FaTwitter size={30} alt="social-icon" /></a>
+                </li>
+                <li>
+                  <a href="www.facebook.com"><FaFacebook size={30} alt="social-icons" /></a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
