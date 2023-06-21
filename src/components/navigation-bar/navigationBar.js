a37c796251d951d4f4486c02d5d4b3ec5b40706d
blase147 <solarmails2@gmail.com>
2023-06-21 08:36:22 +0100

    refactor to SPA

--- src/components/navigation-bar/navigationBar.js
+++ src/components/navigation-bar/navigationBar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { RiMenuLine, RiCloseLine } from 'react-icons/ri';
 import './navigationBar.scss';
 
@@ -37,31 +36,53 @@ const NavigationBar = () => {
     );
   }
 
+  const handleHashLinkClick = (event, url) => {
+    event.preventDefault();
+    const targetId = url.substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const { offsetTop } = targetElement;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth',
+      });
+    }
+    toggleMenu(); // Close the menu after clicking on a link
+  };
+
   return (
     <div>
       <div className="navigation-bar-container">
         <div className="menu-icon">{menuIcon}</div>
         {isOpen && (
-        <ul className="navigation-menu">
-          {navLinks.map(({ url, name }) => (
-            <li key={name}>
-              <Link to={url} onClick={toggleMenu}>
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+          <ul className="navigation-menu">
+            {navLinks.map(({ url, name }) => (
+              <li key={name}>
+                <a
+                  href={url}
+                  onClick={(e) => handleHashLinkClick(e, url)}
+                >
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
         )}
       </div>
       <div className="desktop-nav-container">
         {windowWidth > 768 && (
-        <ul className="navigation-menu">
-          {navLinks.map(({ url, name }) => (
-            <li key={name}>
-              <Link to={url}>{name}</Link>
-            </li>
-          ))}
-        </ul>
+          <ul className="navigation-menu">
+            {navLinks.map(({ url, name }) => (
+              <li key={name}>
+                <a
+                  href={url}
+                  onClick={(e) => handleHashLinkClick(e, url)}
+                >
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
         )}
       </div>
     </div>
